623cd1db2943dfc2e43a8ecc47a0d47faa085ced
musaddiq <shaikhmusaddique49@gmail.com>
2024-10-22 12:59:30 +0530

    .

--- SD-Hub-Portal/src/app/app.module.ts
+++ SD-Hub-Portal/src/app/app.module.ts
@@ -5,19 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { SigninComponent } from './signin/signin.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { RegisterComponent } from './register/register.component';
 import { MatStep, MatStepper, MatStepperModule } from '@angular/material/stepper';
 import { MatInput, MatInputModule } from '@angular/material/input';
 import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatCalendar } from '@angular/material/datepicker';
 import { MatCardModule } from '@angular/material/card';
-import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
-    RegistrationComponent,
     RegisterComponent
   ],
   imports: [
