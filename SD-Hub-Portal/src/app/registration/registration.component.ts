eefdf4078aaef2a5be39f238fa0611ec07e44c7a
musaddiq <musaddique78@gmail.com>
2024-10-22 12:04:58 +0530

    .

--- SD-Hub-Portal/src/app/registration/registration.component.ts
+++ SD-Hub-Portal/src/app/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +6,5 @@ import { Component,OnInit } from '@angular/core';
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
-  
-   }
+
+}
